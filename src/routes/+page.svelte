66ce8a2047235d1191900f3b3925713622f998fe
xbucks <jmy.kot@gmail.com>
2023-08-05 21:13:47 -0400

    ui: change hero heading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="h-96 bg-gray-200 grid items-center">
 	<div class="mx-auto max-w-sm gap-10 grid">
-		<h1 class="text-4xl">Acme Sustainability</h1>
+		<h1 class="text-4xl">Acme Ecommerce Store</h1>
 		<p>Discover a World of Sustainable and Eco-Friendly Products</p>
 		<button class="bg-gray-400 p-4 text-white font-bold">Start Shopping</button>
 	</div>
